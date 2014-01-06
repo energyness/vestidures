c4829546377730f6001d3dbcd42023cd2004ba67
Pau Fabregat Pappaterra <paufabregatpappaterra@MacBook-MacBook-Pro-de-Pau.local>
2014-01-06 04:37:42 +0100

    directory changed

---
+++ 7gen/js/my_functions.js
@@ -0,0 +1,44 @@
+/* Coming soon countdown*/
+var newYear = new Date(); 
+newYear = new Date(2014 , 1 - 1, 7); 
+$('#countdown').countdown({until: newYear,
+layout:'{dnn}:{hnn}:{mnn}:{snn}'});
+
+$('#countdown').css({
+  'font-size': '120pt',
+  'font-family': 'Arial Narrow2',
+  'text-align': 'center',
+  'font-weight' : '800'
+});
+
+/*Centra butons discursos */
+$(function(){
+  var coW = $('.container').width();
+  var shW = coW/2 - 105;
+  $('.share-buttons_h').css('margin-left',shW);
+});
+
+
+
+//Social buttons left, padding constant per mantenir el element fixe
+$(function() {        
+    $("#share-buttons").floatingFixed({ padding: 130 });
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
